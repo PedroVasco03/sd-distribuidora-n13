a0a728a953e316c3704ee32aa08f030733e3a044
Pedro Vasco <baltazarpedro092@gmail.com>
2023-12-11 23:36:59 +0100

    organization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./global.css"
-import "./pages/assets/fontawesome-free-6.5.1-web/css/all.css"
+import "./pages/Home/assets/fontawesome-free-6.5.1-web/css/all.css"
 import Head from "next/head";
-import Hero from "./pages/components/hero/Hero";
-import Navbar from "./pages/components/navbar/Navbar";
-import About from "./pages/components/about/About";
-import Price from "./pages/components/price/Price";
-import Contact from "./pages/components/contact/Contact";
-import Services from "./pages/components/servicess/Services";
-import Footer from "./pages/components/footer/Footer";
-
+import Navbar from "./pages/Home/components/navbar/Navbar";
+import Hero from "./pages/Home/components/hero/Hero";
+import Services from "./pages/Home/components/servicess/Services";
+import Price from "./pages/Home/components/price/Price";
+import About from "./pages/Home/components/about/About";
+import Contact from "./pages/Home/components/contact/Contact";
+import Footer from "./pages/Home/components/footer/Footer";
 
 const Home = () =>{
     return(
@@ -17,7 +16,7 @@ const Home = () =>{
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
             </Head>
-            <Navbar/> 
+            <Navbar/>
             <Hero/>
             <Services/>
             <Price/>
