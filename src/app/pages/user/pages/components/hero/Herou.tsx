5b25771ba049cb2e05ecdb808d83ee984d03b117
PedroVasco <baltazarpedro092@gmail.com>
2024-01-12 15:45:25 +0100

    alterations

--- src/app/pages/user/pages/components/hero/Herou.tsx
+++ src/app/pages/user/pages/components/hero/Herou.tsx
@@ -10,7 +10,7 @@ const Herou = () => {
                 <div className="container">
                 <div className="row">
                     <div className="col-lg-12">
-                    <div className="row">
+                    <div className="row"> 
                         <div className="col-lg-6 align-self-center">
                         <div className={style.left_content+" header-text"} data-wow-duration="1s" data-wow-delay="1s">
                             <div className="row">
